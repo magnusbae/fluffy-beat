092338369be20d0ee2deff12f10465981b8ebb1f
Magnus Bae <magnusbae@gmail.com>
2015-11-05 22:25:36 +0100

    Responsive scalable styling powered by reactsomeness and tvs without spacing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,36 @@ import React, { PropTypes, Component } from 'react';
 import styles from './App.css';
 import withContext from '../../decorators/withContext';
 import withStyles from '../../decorators/withStyles';
+import withViewport from '../../decorators/withViewport';
 
 
 import BeatBox from '../BeatBox';
 
 @withContext
+@withViewport
 @withStyles(styles)
 class App extends Component {
 
   static propTypes = {
     children: PropTypes.element.isRequired,
     error: PropTypes.object,
+    viewport: PropTypes.shape({
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired,
+    }).isRequired,
   };
 
   render() {
+    const width = this.props.viewport.width / 5;
+    const height = width * 0.879;
+
+    this.renderCss(`.BeatBox-container { width: ${width}px; height: ${height}px; } ` +
+        `.BeatBox-text { \
+        font-size: ${height/5}px; \
+        margin-top: ${height/30}px; \
+        padding-bottom: ${height/30}px; \
+        }`);
+
     return !this.props.error ? (
       <div>
         <BeatBox content="every" />
