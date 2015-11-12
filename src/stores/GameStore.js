628be7af0f258c10be5882551ff9485419a86c7b
Magnus Bae <magnusbae@gmail.com>
2015-11-12 23:10:27 +0100

    Made store into ES6 class

--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -1,54 +1,62 @@
 import dispatcher from '../core/Dispatcher';
-import events, {EventEmitter} from 'events';
+import EventEmitter from 'events';
 import _ from 'lodash';
 
 import ActionTypes from '../constants/ActionTypes';
-var CHANGE_EVENT = 'change';
 
-var _currentWords = null;
+const CHANGE_EVENT = 'change';
+let _currentWords = [];
+let _displayedScreens = {};
 
-var GameStore = _.assign({}, EventEmitter.prototype, {
-
-  init: function (strofe) {
-    if(strofe){
+class GameStore extends EventEmitter {
+  
+  init(strofe) {
+    if (strofe) {
       _currentWords = strofe.split(' ');
+      _displayedScreens = {};
+      _.forEach(_currentWords, (word, index) => {
+        _displayedScreens[index] = false;
+      });
     }
-  },
-
-  getWords: function(){
-    return _currentWords;
-  },
-
-  emitChange: function () {
-    this.emit(CHANGE_EVENT);
-  },
-
-  /**
-   * @param {function} callback
-   */
-  addChangeListener: function (callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
-
-  /**
-   * @param {function} callback
-   */
-  removeChangeListener: function (callback) {
-    this.removeListener(CHANGE_EVENT, callback);
   }
 
-});
+  getWords(){
+    return _currentWords;
+  }
 
+  emitChange () {
+    this.emit(CHANGE_EVENT);
+  }
 
-GameStore.dispatchToken = dispatcher.register(function (action) {
+  /**
+   * @param {function} callback
+   */
+  addChangeListener (callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+
+  /**
+   * @param {function} callback
+   */
+  removeChangeListener (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+}
+
+const store = new GameStore();
+
+store.dispatchToken = dispatcher.register(function (action) {
   switch (action.type) {
     case ActionTypes.STROFE_CHANGED:
-      GameStore.init(action.strofe);
-      GameStore.emitChange();
+      store.init(action.strofe);
+      store.emitChange();
       break;
+    case ActionTypes.SHOW_SCREEN:
+      _displayedScreens[action.screen] = true;
+      store.emitChange();    
     default:
     //do nothing
   }
 });
 
-export default GameStore;
+export default store;
